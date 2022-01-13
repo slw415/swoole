407532d96a9de808bc4012b095b7a8e2ccd6b1ca
shiliwei <17858024722@163.com>
2022-01-13 22:04:02 +0800

    第一次提交

--- demo/server/tcp.php
+++ demo/server/tcp.php
@@ -1,1 +1,19 @@
 <?php
+
+$server = new Swoole\Server('127.0.0.1', 9501);
+
+$server->on('Connect', function ($server, $d){
+    echo "Client:Connect.\n";
+});
+
+//监听数据接收事件
+$server->on('Receive', function ($server, $fd, $reactor_id, $data){
+   $server->send($fd, "Server:{$data}");
+});
+
+//监听链接关闭事件
+$server->on('Close',function ($server, $fd){
+    echo "Clinent:Close.\n";
+});
+//启动服务器
+$server->start();
