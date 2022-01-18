eb464b5da25dfdf9187e2268694b54b93dcc1a04
shiliwei <17858024722@163.com>
2022-01-18 21:01:52 +0800

    第一次提交

---
+++ demo/client/tcp.php
@@ -0,0 +1,15 @@
+<?php
+//链接swoole tcp 服务
+$client = new Swoole\Client(SWOOLE_SOCK_TCP);
+if (!$client->connect('127.0.0.1', 9501, -1)){
+    echo "链接失败";
+    exit;
+}
+//php cli 常量
+fwrite(STDOUT,'请输入消息:');
+$msg = trim(fgetc(STDIN));
+
+//发送消息 给 tcp server 服务器
+$client->send($msg);
+//接收 来自server 的数据
+$res = $client->recv();
