765e3d5deb90b2829baf4841539fdd04d0386a1e
shiliwei <17858024722@163.com>
2022-01-19 22:02:45 +0800

    第一次提交

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -4,4 +4,4 @@ $http ->on('Request', function ($request,$response){
     $response->header('Content-Type', 'text/html; charset=utf-8');
     $response->end('<h1>Hello Swoole. #' . rand(1000, 9999) . '</h1>');
 });
-$http->send();
+$http->start();
