88144f3f17b1a844686eb331d9382d5ae2060a66
shiliwei <17858024722@163.com>
2022-01-19 22:08:52 +0800

    第一次提交

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -2,6 +2,7 @@
 $http = new Swoole\Http\Server('0.0.0.0', 8811);
 $http ->on('Request', function ($request,$response){
     $response->header('Content-Type', 'text/html; charset=utf-8');
+    print_r($request->get);
     $response->end('<h1>Hello Swoole. #' . rand(1000, 9999) . '</h1>');
 });
 $http->start();
