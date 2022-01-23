cb6a900282faf7d41a56780575593f6ed7d78ab7
shiliwei <17858024722@163.com>
2022-01-23 16:53:57 +0800

    第一次提交

--- demo/server/ws_server.php
+++ demo/server/ws_server.php
@@ -1,6 +1,10 @@
 <?php
 $ws = new Swoole\WebSocket\Server('0.0.0.0', 9502);
-
+$ws->set(
+    [
+        'enable_static_handler' => true,
+        'document_root' => '/home/work/study/swoole_mooc/swoole/data'
+    ];
 //监听WebSocket连接打开事件
 $ws->on('Open', function ($ws, $request) {
     $ws->push($request->fd, "hello, welcome\n");
