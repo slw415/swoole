23832245fe5e2c0af31841386c37812babc714e8
shiliwei <17858024722@163.com>
2022-01-23 19:31:38 +0800

    第一次提交

--- demo/server/ws.php
+++ demo/server/ws.php
@@ -11,12 +11,16 @@ class Ws{
         $this->ws = new Swoole\WebSocket\Server('0.0.0.0', 9502);
         $this->ws->set(
             [
+                'worker_num' =>2,
+                'task_worker_num' =>2,
                 'enable_static_handler' => true,
                 'document_root' => '/home/work/study/swoole_mooc/swoole/data'
             ]);
 
         $this->ws->on("open",[$this,'onOpen']);
         $this->ws->on("message", [$this,'onMessage']);
+        $this->ws->on("task", [$this,'onTask']);
+        $this->ws->on("finish", [$this,'onFinish']);
         $this->ws->on("close",[$this,'onClose']);
         $this->ws->start();
     }
@@ -43,9 +47,37 @@ class Ws{
      */
     public function onMessage($ws, $frame){
         echo "ser-push-message:{$frame->data}\n";
+        //todo 10s
+        $data = [
+          'task' => 1,
+          'fd' => $frame->fd
+        ];
+        $ws->task();
         $ws->push($frame->fd, "server-push:".date("Y-m-d H:i:s"));
     }
 
+    /**
+     *DESC:
+     *AUTHOR:余杭胡歌
+     *DATE:2022/1/23
+     *TIME:7:21 下午
+     * @param $serv
+     * @param $taskId
+     * @param $workId
+     * @param $data
+     */
+    public function onTask($serv, $taskId, $workId, $data){
+        print_r($data);
+        //耗时场景
+        sleep(10);
+        return "on task finish";//告诉worker进程
+    }
+
+    public function onFinish($serv,$taskId,$data){
+        echo "taskId:{$taskId}\n";
+        echo "finish-data-success:{$data}";
+
+    }
     /**
      *DESC:ws 关闭事件
      *AUTHOR:余杭胡歌
