c8503c69f01d3575ea453b8e0d70e3db68de1284
shiliwei <17858024722@163.com>
2022-02-01 20:54:45 +0800

    222

--- demo/process/process.php
+++ demo/process/process.php
@@ -2,7 +2,7 @@
 use Swoole\Process;
 $process = new Process(function (Process $pro){
     //todo
-       $pro->exec("/home/work/study/soft/php/bin/php",[__DIR__.'./server/http_server.php']);
+       $pro->exec("/home/work/study/soft/php/bin/php",[__DIR__.'/./server/http_server.php']);
 },false);
 $pid = $process->start();
 echo  $pid.PHP_EOL;
