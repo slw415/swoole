83e072fc2ee82fa77a623fa21e8c0ec254edded3
shiliwei <17858024722@163.com>
2022-02-02 17:36:57 +0800

    222

--- demo/coroutine/redis.php
+++ demo/coroutine/redis.php
@@ -4,5 +4,6 @@ use function Swoole\Coroutine\run;
 run(function () {
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
-    $val = $redis->get('key');
+    $redis->set('key',2222);
+    var_dump($redis->get('key'));
 });
