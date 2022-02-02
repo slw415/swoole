ae297fc3802248ecf789a257875fda87703ab4c1
shiliwei <17858024722@163.com>
2022-02-02 16:08:04 +0800

    222

--- demo/memory/table.php
+++ demo/memory/table.php
@@ -4,7 +4,7 @@ use Swoole\Table;
 $table = new Table(1024);
 //内存表增加一行
 $table->column('id',$table::TYPE_INT, 4);
-$table->column('name', $table::TYPE_STRING64, );
+$table->column('name', $table::TYPE_STRING,64 );
 $table->column('age', $table::TYPE_INT, 3);
 $table->create();
 
