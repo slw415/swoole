a5b21c881421e414c28521a9ee4fc2c53b56b40e
shiliwei <17858024722@163.com>
2022-03-26 17:17:34 +0800

    222

--- demo/process/ceshi.php
+++ demo/process/ceshi.php
@@ -3,12 +3,10 @@ use function Swoole\Coroutine\run;
 
 run(function (){
 
-    sleep(1);
+    Swoole\Coroutine\System::sleep(1);
     echo 2;
 });
 echo 3;
 run(function (){
-
-    sleep(1);
     echo 1;
 });
